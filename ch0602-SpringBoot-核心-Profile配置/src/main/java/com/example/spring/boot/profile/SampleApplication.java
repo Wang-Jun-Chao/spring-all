e87c48895246ed337a4625a9f22e96680c103ce7
王俊超 <shining-glory@qq.com>
2017-07-14 21:43:52 +0800

    commit

---
+++ ch0602-SpringBoot-核心-Profile配置/src/main/java/com/example/spring/boot/profile/SampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.spring.boot.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-07-14 21:11
+ * All Rights Reserved !!!
+ */
+@SpringBootApplication
+public class SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
