b56b3df1a7a8f3d940849eec4010473625e64741
王俊超 <shining-glory@qq.com>
2017-07-16 21:33:13 +0800

    commit

--- ch0706-SpringBoot-Web-SSL配置/src/main/java/com/example/spring/boot/ssl/SampleApplication.java
+++ ch0706-SpringBoot-Web-SSL配置/src/main/java/com/example/spring/boot/ssl/SampleApplication.java
@@ -1,8 +1,20 @@
 package com.example.spring.boot.ssl;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.security.util.SecurityConstants;
 
 /**
  * Author: 王俊超
@@ -20,5 +32,36 @@ public class SampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory containerFactory () {
+        TomcatEmbeddedServletContainerFactory factory =
+                new TomcatEmbeddedServletContainerFactory() {
+                    @Override
+                    protected void postProcessContext(Context context) {
+                        SecurityConstraint constraint = new SecurityConstraint();
+                        constraint.setUserConstraint("CONFIDENTIAL");
+                        SecurityCollection collection = new SecurityCollection();
+                        collection.addPattern("/*");
+                        constraint.addCollection(collection);
+                        context.addConstraint(constraint);
+                    }
+                };
+
+        factory.addAdditionalTomcatConnectors(httpConnector());
+
+        return factory;
+    }
+
+    private Connector httpConnector() {
+        System.out.println(Http11NioProtocol.class.getName());
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
     }
 }
