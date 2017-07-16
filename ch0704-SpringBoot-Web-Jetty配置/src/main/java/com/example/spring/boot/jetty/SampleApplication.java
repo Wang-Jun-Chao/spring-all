2437266c7b384f20d924b773fbf888077c93f327
王俊超 <shining-glory@qq.com>
2017-07-16 20:22:31 +0800

    commit

---
+++ ch0704-SpringBoot-Web-Jetty配置/src/main/java/com/example/spring/boot/jetty/SampleApplication.java
@@ -0,0 +1,24 @@
+package com.example.spring.boot.jetty;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-07-16 20:05
+ * All Rights Reserved !!!
+ */
+@RestController
+@SpringBootApplication
+public class SampleApplication {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+}
