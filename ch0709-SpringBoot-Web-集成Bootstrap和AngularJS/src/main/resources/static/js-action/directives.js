247938b50366fd6e1c560e8081d344511f0e3f66
王俊超 <shining-glory@qq.com>
2017-07-17 08:16:51 +0800

    commit

---
+++ ch0709-SpringBoot-Web-集成Bootstrap和AngularJS/src/main/resources/static/js-action/directives.js
@@ -0,0 +1,12 @@
+actionApp.directive('datePicker', function () {
+    return {
+        restrict: 'AC',
+        link: function (scope, elem, attrs) {
+            // scope.treeObj = $.fn.zTree.init(elem, scope.settings);
+
+            elem.datepicker();
+        }
+    };
+});
+
+
