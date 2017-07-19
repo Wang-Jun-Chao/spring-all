c37c9bd8db45cf1eda6f855ccb8968c20d9a9cec
王俊超 <shining-glory@qq.com>
2017-07-20 07:43:37 +0800

    commit

---
+++ ch0902-SpringBoot-企业级开发-SpringBatch/src/main/java/com/example/spring/boot/SampleApplication.java
@@ -0,0 +1,9 @@
+package com.example.spring.boot;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-07-20 07:43
+ * All Rights Reserved !!!
+ */
+public class SampleApplication {
+}
---
+++ ch0902-SpringBoot-企业级开发-SpringBatch/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PERSON (
+  id NUMBER NOT NULL PRIMARY KEY,
+  name        VARCHAR2(20),
+  age NUMBER,
+  nation      VARCHAR2(20),
+  address     VARCHAR2(20)
+);
+
