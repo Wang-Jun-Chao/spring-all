71de02ef55b667bdfccaab3009da97c8ecb3eb1d
王俊超 <shining-glory@qq.com>
2017-07-25 08:10:25 +0800

    commit

--- ch0102-Spring基础-Java配置/src/main/java/com/example/spring/framework/javaconfig/FunctionService.java
+++ ch0102-Spring基础-Java配置/src/main/java/com/example/spring/framework/javaconfig/FunctionService.java
@@ -3,8 +3,8 @@ package com.example.spring.framework.javaconfig;
 import org.springframework.stereotype.Service;
 
 /**
- * 使用@Service 注解声明当前FunctionService 类是Spring 管理的一个Beano 其中，使
- * 用@Component 、@Service 、@Repository 和@Controller 是等效的，可根据需要选用。
+ * 使用@Service注解声明当前FunctionService 类是Spring 管理的一个Beano 其中，使
+ * 用@Component、@Service、@Repository和@Controller 是等效的，可根据需要选用。
  *
  * Author: 王俊超
  * Date: 2017-07-10 08:01
--- ch0102-Spring基础-Java配置/src/main/java/com/example/spring/framework/javaconfig/JavaConfig.java
+++ ch0102-Spring基础-Java配置/src/main/java/com/example/spring/framework/javaconfig/JavaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 1、@Configuration 声明当前类是一个配置类
- * 2、@Bean 将方法的返回值当作一个bean，bean的名称就是返回值的名称
+ * 2、@Bean将方法的返回值当作一个bean，bean的名称就是返回值的名称
  * 3、使用手动注入
  * 4、使用容器自动注入
  *
@@ -14,9 +14,9 @@ import org.springframework.context.annotation.Configuration;
  * All Rights Reserved !!!
  */
 
-@Configuration //1
+@Configuration // 1
 public class JavaConfig {
-    @Bean //2
+    @Bean // 2
     public FunctionService functionService() {
         return new FunctionService();
     }
@@ -24,13 +24,12 @@ public class JavaConfig {
     @Bean
     public UseFunctionService useFunctionService() {
         UseFunctionService useFunctionService = new UseFunctionService();
-        useFunctionService.setFunctionService(functionService()); //3
+        useFunctionService.setFunctionService(functionService()); // 3
         return useFunctionService;
-
     }
 
     @Bean
-    public UseFunctionService useFunctionService2(FunctionService functionService) {//4
+    public UseFunctionService useFunctionService2(FunctionService functionService) {// 4
         UseFunctionService useFunctionService = new UseFunctionService();
         useFunctionService.setFunctionService(functionService);
         return useFunctionService;
