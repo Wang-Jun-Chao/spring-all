586facdf7a2f90521743fda24b951c0123c175c1
王俊超 <shining-glory@qq.com>
2017-07-26 08:05:19 +0800

    commit

--- ch0201-Spring常用配置-Bean的Scope/src/main/java/com/example/spring/framework/scope/DemoSingletonService.java
+++ ch0201-Spring常用配置-Bean的Scope/src/main/java/com/example/spring/framework/scope/DemoSingletonService.java
@@ -3,6 +3,7 @@ package com.example.spring.framework.scope;
 import org.springframework.stereotype.Service;
 
 /**
+ * 默认为Singleton ，相当于＠Scope（“singleton”）
  * Author: 王俊超
  * Date: 2017-07-10 22:38
  * All Rights Reserved !!!
