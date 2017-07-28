ed0ea2b28728635e9b3543b645a8119a3a1ef517
王俊超 <shining-glory@qq.com>
2017-07-29 07:00:44 +0800

    commit

--- ch0303-Spring高级话题-计划任务/src/main/java/com/example/spring/framework/taskscheduler/ScheduledTaskService.java
+++ ch0303-Spring高级话题-计划任务/src/main/java/com/example/spring/framework/taskscheduler/ScheduledTaskService.java
@@ -7,6 +7,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
+ * ①通过＠Scheduled 声明该方法是计划任务，使用fixedRate 属性每隔固定时间执行。
+ * ②使用cron 属性可按照指定时间执行，本例指的是每天11 点28 分执行；
+ * cron 是UNIX和类UNIX ( Linux ）系统下的定时任务。
  * Author: 王俊超
  * Date: 2017-07-11 08:04
  * All Rights Reserved !!!
@@ -20,8 +23,8 @@ public class ScheduledTaskService {
         System.out.println("每隔五秒执行一次 " + dateFormat.format(new Date()));
     }
 
-    @Scheduled(cron = "0 28 11 ? * *"  ) //2
-    public void fixTimeExecution(){
-        System.out.println("在指定时间 " + dateFormat.format(new Date())+"执行");
+    @Scheduled(cron = "0 28 11 ? * *") //2
+    public void fixTimeExecution() {
+        System.out.println("在指定时间 " + dateFormat.format(new Date()) + "执行");
     }
 }
