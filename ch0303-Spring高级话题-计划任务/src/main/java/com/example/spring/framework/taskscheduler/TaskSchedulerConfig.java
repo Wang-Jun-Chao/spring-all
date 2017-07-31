132b571e1fccc32ce78337c0f90834c2ca0447f0
王俊超 <shining-glory@qq.com>
2017-07-31 08:07:28 +0800

    commit

--- ch0303-Spring高级话题-计划任务/src/main/java/com/example/spring/framework/taskscheduler/TaskSchedulerConfig.java
+++ ch0303-Spring高级话题-计划任务/src/main/java/com/example/spring/framework/taskscheduler/TaskSchedulerConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * ①通过＠EnableScheduling 注解开启对计划任务的支持。
+ * ①通过@EnableScheduling 注解开启对计划任务的支持。
  * Author: 王俊超
  * Date: 2017-07-11 08:04
  * All Rights Reserved !!!
