b4ef5dc9422acac71003ff573b97f9a4bdc6bb10
王俊超 <shining-glory@qq.com>
2017-08-03 08:24:39 +0800

    commit

--- ch0403-SpringMVC基础-基本配置/src/main/java/com/example/spring/framework/mvc/configuration/controller/AdviceController.java
+++ ch0403-SpringMVC基础-基本配置/src/main/java/com/example/spring/framework/mvc/configuration/controller/AdviceController.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdviceController {
     @RequestMapping("/advice")
     public String getSomething(@ModelAttribute("msg") String msg, DemoObj obj) {
-        throw new IllegalArgumentException("非常抱歉，参数有误/"+"来自@ModelAttribute:"+ msg);
+        throw new IllegalArgumentException("非常抱歉，参数有误/" + "来自@ModelAttribute:" + msg);
     }
 }
