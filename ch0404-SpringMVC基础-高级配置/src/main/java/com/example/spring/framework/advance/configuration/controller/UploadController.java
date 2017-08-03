9ba75cce2494794863e9b0f66eaa5c70b82905fc
王俊超 <shining-glory@qq.com>
2017-08-04 07:41:17 +0800

    commit

--- ch0404-SpringMVC基础-高级配置/src/main/java/com/example/spring/framework/advance/configuration/controller/UploadController.java
+++ ch0404-SpringMVC基础-高级配置/src/main/java/com/example/spring/framework/advance/configuration/controller/UploadController.java
@@ -11,18 +11,21 @@ import java.io.File;
 import java.io.IOException;
 
 /**
+ * ①使用MultipartFile file 接受上传的文件。
+ * ②使用FileUtils. writeByteArrayToFile 快速写文件到磁盘。
  * Author: 王俊超
  * Date: 2017-07-12 07:56
  * All Rights Reserved !!!
  */
 @Controller
 public class UploadController {
-    @RequestMapping(value = "/upload",method = RequestMethod.POST)
-    public @ResponseBody String upload(MultipartFile file) {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody
+    String upload(MultipartFile file) { // 1
         try {
             FileUtils.writeByteArrayToFile(
-                    new File("e:/upload/"+file.getOriginalFilename()),
-                    file.getBytes()); //2
+                    new File("e:/upload/" + file.getOriginalFilename()),
+                    file.getBytes()); // 2
             return "ok";
         } catch (IOException e) {
             e.printStackTrace();
