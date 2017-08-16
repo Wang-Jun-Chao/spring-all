5abf29fb5bf23029764ac44bed7a8386b1ad40c2
王俊超 <shining-glory@qq.com>
2017-08-17 07:48:15 +0800

    commit

--- ch0601-SpringBoot-核心-类型安全的配置/src/main/java/com/example/spring/boot/safetype/AuthorSettings.java
+++ ch0601-SpringBoot-核心-类型安全的配置/src/main/java/com/example/spring/boot/safetype/AuthorSettings.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
+ * 通过@ConfigurationProperties 加载properties 文件内的配置，通过prefix 属性指定
+ * properties 的配置的前缀，
  * Author: 王俊超
  * Date: 2017-07-14 21:01
  * All Rights Reserved !!!
