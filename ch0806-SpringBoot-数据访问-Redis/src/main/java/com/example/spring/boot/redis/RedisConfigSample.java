4032e4a2c7645f256da2de85cb107abab6ca1f23
王俊超 <shining-glory@qq.com>
2017-08-27 14:13:26 +0800

    commit

--- ch0806-SpringBoot-数据访问-Redis/src/main/java/com/example/spring/boot/redis/RedisConfigSample.java
+++ ch0806-SpringBoot-数据访问-Redis/src/main/java/com/example/spring/boot/redis/RedisConfigSample.java
@@ -1,26 +1,26 @@
-package com.example.spring.boot.redis;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-
-/**
- * Author: 王俊超
- * Date: 2017-08-27 13:58
- * All Rights Reserved !!!
- */
-public class RedisConfigSample {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
-
-    @Bean
-    public RedisTemplate<Object, Object> redisTemplate() {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
-        return template;
-    }
-
-}
+//package com.example.spring.boot.redis;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+///**
+// * Author: 王俊超
+// * Date: 2017-08-27 13:58
+// * All Rights Reserved !!!
+// */
+//public class RedisConfigSample {
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new JedisConnectionFactory();
+//    }
+//
+//    @Bean
+//    public RedisTemplate<Object, Object> redisTemplate() {
+//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(redisConnectionFactory());
+//        return template;
+//    }
+//
+//}
