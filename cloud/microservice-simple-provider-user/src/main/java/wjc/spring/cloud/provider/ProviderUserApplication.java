78cab569b860486ec833471a404acac0d429dcc8
王俊超 <wangjunchao@gf.com.cn>
2018-12-26 15:42:39 +0800

    commit

---
+++ cloud/microservice-simple-provider-user/src/main/java/wjc/spring/cloud/provider/ProviderUserApplication.java
@@ -0,0 +1,17 @@
+package wjc.spring.cloud.provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-12-26 14:06
+ **/
+@SpringBootApplication
+public class ProviderUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderUserApplication.class, args);
+    }
+}
