530801790c9b1390713e4a58e9dd5c3438b7419e
王俊超 <wangjunchao@gf.com.cn>
2019-01-04 16:22:02 +0800

    commit

--- cloud/020-hystrix-feign-rolback-factory/provider-user-hystrix-feign-rolback-factory/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (id, username, name, age, balance) VALUES (1, 'account1', '张三', 20, 100.00);
-INSERT INTO user (id, username, name, age, balance) VALUES (2, 'account2', '李四', 28, 180.00);
-INSERT INTO user (id, username, name , age, balance) VALUES (3, 'account3', '王五', 32, 280.00);
