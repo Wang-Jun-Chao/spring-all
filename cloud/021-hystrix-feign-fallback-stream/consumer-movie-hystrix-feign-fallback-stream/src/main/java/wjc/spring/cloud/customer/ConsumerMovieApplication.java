8a27390aa44b27b68aae1e34f77f2c3b97865507
王俊超 <wangjunchao@gf.com.cn>
2019-01-04 16:22:29 +0800

    commit

---
+++ cloud/021-hystrix-feign-fallback-stream/consumer-movie-hystrix-feign-fallback-stream/src/main/java/wjc/spring/cloud/customer/ConsumerMovieApplication.java
@@ -0,0 +1,26 @@
+package wjc.spring.cloud.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: wangjunchao(王俊超)
+ * @time: 2018-12-26 14:06
+ **/
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerMovieApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+}
