028863c1fe84222c590acc7c7a7936fa198b4ff9
王俊超 <wangjunchao@gf.com.cn>
2019-01-04 15:20:08 +0800

    commit

---
+++ cloud/019-hystrix-feign/provider-user-hystrix-feign/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user (id, username, name, age, balance) VALUES (1, 'account1', '张三', 20, 100.00);
+INSERT INTO user (id, username, name, age, balance) VALUES (2, 'account2', '李四', 28, 180.00);
+INSERT INTO user (id, username, name , age, balance) VALUES (3, 'account3', '王五', 32, 280.00);
